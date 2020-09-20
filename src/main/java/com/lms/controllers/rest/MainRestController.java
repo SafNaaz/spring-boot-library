4f17ca10f154f1d95111e2e6f7c91d085e635472
Safnas <iamsafnas@gmail.com>
2020-09-20 18:12:06 +0530

    deleting book using request param id

--- src/main/java/com/lms/controllers/rest/MainRestController.java
+++ src/main/java/com/lms/controllers/rest/MainRestController.java
@@ -4,6 +4,7 @@ import com.lms.models.Book;
 import com.lms.services.LmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
@@ -22,5 +23,10 @@ public class MainRestController {
     @GetMapping("/findAllBooks")
     public Collection<Book> getAllBooks(){
         return lmsService.findAllBooks();
+    }
+
+    @GetMapping("/delete")
+    public void deleteBook(@RequestParam long id){
+        lmsService.delete(id);
     }
 }
--- src/main/java/com/lms/services/LmsService.java
+++ src/main/java/com/lms/services/LmsService.java
@@ -22,4 +22,8 @@ public class LmsService {
         }
         return books;
     }
+
+    public void delete(long id){
+        lmsRepository.deleteById(id);
+    }
 }
